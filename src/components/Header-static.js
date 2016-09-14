513602211dcbd1fe49d99430f3a461024d31d58f
Roberto Chavez <r_a_chavez@yahoo.com>
2016-09-14 13:25:04 -0700

    missing files may be issue. Create them.

---
+++ src/components/Header-static.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+const Header = () => <h1>Hello, World!</h1>
+
+export default Header
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from '.src/Header-static'
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('main-view')
+)

53a6596c450ccff1231c35ed093ee2f708795c52
Roberto Chavez <r_a_chavez@yahoo.com>
2016-09-14 20:19:56 -0700

    updated webpack config file with new loaders. New loaders included in package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,20 @@ module.exports = {
     },
     module: {
         loaders: [
+            {   test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: 'babel' 
+            },
+            {   test: /\.css$/, 
+                loader: "style-loader!css-loader" 
+            },
+            {   test: /\.(ttf|eot|otf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
+                loader: 'file'
+            },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel'
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+
             }
         ]
     }

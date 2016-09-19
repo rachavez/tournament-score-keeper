150383f1a6b92c06e84d0130d5ed6643e5f9538d
Roberto Chavez <r_a_chavez@yahoo.com>
2016-09-19 14:38:25 -0700

    create footer file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,27 @@
 import React from 'react'
+
+const propTypes = {
+
+};
+
+export default class Footer extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <footer class="footer">
+                <div class="container">
+                    <p class="text-muted">This website for entertainment purposes only. &copy; 2016 Chavez Designs</p>
+                </div>
+            </footer>
+        );
+    }
+} 
+
+Navbar.propTypes = propTypes

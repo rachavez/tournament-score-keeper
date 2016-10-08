a542a6ec169549ff57ca18ebcc4b3d6cd41a63b7
Roberto Chavez <r_a_chavez@yahoo.com>
2016-10-08 16:23:29 -0700

    duplicated two files and used appropriate file names

---
+++ src/components/Header-new.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const propTypes = {
+
+};
+
+export default class Navbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-default navbar-fixed-top">
+                <div className="container-fluid">
+                    <div className="navbar-header">
+                        <a className="navbar-brand" href="./index.html">Score Keeper</a>
+                    </div>
+                </div> 
+            </nav>
+        );
+    }
+}
+
+Navbar.propTypes = propTypes
---
+++ src/components/Placeholder.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+
+const Header = () => <h1>Hello, World from React and MainView!</h1>
+
+export default Header

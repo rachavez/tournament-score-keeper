442b72a31fab2a3d53f10088abe391207ffe0deb
Roberto Chavez <r_a_chavez@yahoo.com>
2016-10-09 15:37:25 -0700

    second try at MainView-new.js

--- src/components/MainView-new.js
+++ src/components/MainView-new.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react'
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import Placeholder from './Placeholder'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
-class MainView extends Component {
+const propType = {
+
+};
+
+const Container = (props) => <div>
+    <Navbar />
+    <div className="container-fluid">
+        {props.children}
+    </div>
+    <Footer />
+</div>
+
+const NotFound = () => <h1>404. This page is not found! </h1>
+
+export default class MainView extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <Router history={hashHistory} >
-                <Route path='/' component={Home} />
-                <Route path='/address' component={Address} />
+                <Route path='/' component={Container}>
+                    <IndexRoute path={Placeholder} />
+                    <Route path='*' component={NotFound} />
+                </Route>
             </Router>
         )
     }
